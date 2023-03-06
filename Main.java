2c60505bb27c201bf483742c2646295852fb6472
Irlond100 <121184697+Irlond100@users.noreply.github.com>
2023-03-06 14:35:59 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,18 +11,27 @@ import java.util.zip.ZipOutputStream;
 public class Main {
 	
 	public static void main(String[] args) {
-		saveGame("save1.txt",new GameProgress(1,1,1,2));
-		saveGame("save2.txt",new GameProgress(2,2,2,2));
-		saveGame("save3.txt",new GameProgress(3,3,3,3));
+		saveGame("save1.txt", new GameProgress(1, 1, 1, 2));
+		saveGame("save2.txt", new GameProgress(2, 2, 2, 2));
+		saveGame("save3.txt", new GameProgress(3, 3, 3, 3));
 		
-		zipFiles("zip1.zip","save1.txt");
-		zipFiles("zip2.zip","save2.txt");
-		zipFiles("zip3.zip","save3.txt");
+		zipFiles("zip1.zip", "save1.txt");
+		zipFiles("zip2.zip", "save2.txt");
+		zipFiles("zip3.zip", "save3.txt");
+		
+		del("save1.txt");
+		del("save2.txt");
+		del("save3.txt");
+	}
+	
+	private static void del(String s) {
+		File f = new File(s);
+		f.delete();
 	}
 	
 	private static void zipFiles(String s, String s1) {
 		try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(s));
-			FileInputStream file = new FileInputStream(s1))
+			 FileInputStream file = new FileInputStream(s1))
 		{
 			ZipEntry z = new ZipEntry(s1);
 			zip.putNextEntry(z);
@@ -30,18 +39,15 @@ public class Main {
 			file.read(buffer);
 			zip.write(buffer);
 			zip.closeEntry();
-		
-		
 			
 		} catch (IOException ex) {
 			
 			System.out.println(ex.getMessage());
 		}
-		 File f =new File(s1);
-		f.delete();
+		
 	}
 	
-	private static void saveGame(String s,GameProgress g) {
+	private static void saveGame(String s, GameProgress g) {
 		File file = new File(s);
 		try {
 			if (file.createNewFile()) {
